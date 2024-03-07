24064c247959d04730b92363054e780df68447e7
Muthu kesavan S <muthukesavan6044@gmail.com>
2024-03-07 19:05:45 +0530

    Final Commit

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,6 @@ import express from "express";
 import { signup, signin } from '../controllers/auth.js';
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/', signup);
 router.post('/signin', signin);
 export default router;
